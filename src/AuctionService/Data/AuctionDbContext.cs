4087ba07dc5abed9af7b7f977b5336c4e5f2ed1c
Shahin al kabir <mitul.cse@gmail.com>
2024-10-05 15:30:10 +0600

    Working on Auction Service

---
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -0,0 +1,11 @@
+using AuctionService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Data;
+
+public class AuctionDbContext(DbContextOptions options) : DbContext(options)
+{
+  
+
+    public DbSet<Auction> Auctions { get; set; }
+}
