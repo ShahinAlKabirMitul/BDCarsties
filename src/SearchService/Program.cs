f1675be288cafef7d6e8bf25840c48398cb6f127
Shahin al kabir <mitul.cse@gmail.com>
2024-10-07 20:20:41 +0600

    Working on http communication resilient using Polly

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,7 @@
+using System.Net;
 using MongoDB.Driver;
-using MongoDB.Entities;
+using MongoDB.Entities;using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Models;
 using SearchService.Services;
@@ -11,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -21,16 +23,24 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
 
-try
-{
-   await DbInitializer.InitDb(app);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-   
-}
+
 
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage>GetPolicy()
+    => HttpPolicyExtensions.HandleTransientHttpError()
+        .OrResult(msg=> msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_=> TimeSpan.FromSeconds(3));
