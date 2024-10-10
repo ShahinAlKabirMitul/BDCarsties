ae1d79d2e0cf41be07e27c1238ee19eb6f78261f
Shahin al kabir <mitul.cse@gmail.com>
2024-10-10 15:13:56 +0600

    working on message outbox

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -57,9 +57,10 @@ public class AuctionsController: ControllerBase
         var auction = _mapper.Map<Auction>(createAuctionDto);
         auction.Seller = "Mitul";
         _context.Auctions.Add(auction);
-        var result = await _context.SaveChangesAsync() > 0;
         var newAuction = _mapper.Map<AuctionDto>(auction);
         await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+        var result = await _context.SaveChangesAsync() > 0;
+       
         if (!result)
         {
             return BadRequest("Could not save to Db");
