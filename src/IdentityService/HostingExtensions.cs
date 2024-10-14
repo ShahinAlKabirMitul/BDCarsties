d17d5e26b7b1cba16b4572d3f7c54755b7fa86b6
Shahin al kabir <mitul.cse@gmail.com>
2024-10-14 20:05:08 +0600

    working on seeding data & migration of Identity service

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -35,6 +35,10 @@ internal static class HostingExtensions
             .AddInMemoryClients(Config.Clients)
             .AddAspNetIdentity<ApplicationUser>();
 
+        builder.Services.ConfigureApplicationCookie(option =>
+        {
+            option.Cookie.SameSite = SameSiteMode.Lax;
+        });
         builder.Services.AddAuthentication();
             
 
