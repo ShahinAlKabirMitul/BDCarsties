c08df7a69cbde02058615ee14f2166790b36a891
Shahin al kabir <mitul.cse@gmail.com>
2024-10-20 20:23:32 +0600

    working on gateway service

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -14,14 +14,14 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username";
     });
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("customPolicy", b =>
-    {
-        b.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
-            .WithOrigins(builder.Configuration["ClientApp"]!);
-    });
-});
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("customPolicy", b =>
+//     {
+//         b.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+//             .WithOrigins(builder.Configuration["ClientApp"]!);
+//     });
+// });
 
 var app = builder.Build();
 
